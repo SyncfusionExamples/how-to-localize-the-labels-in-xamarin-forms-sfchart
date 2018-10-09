a0d4def5c7faca8de5280198efb4a1372c21a2dd
ManivannanE <manivannanelangovan@syncfusion.com>
2018-10-09 17:48:39 +0530

    Localization sample committed for all platforms.

---
+++ SfChartLocalization/SfChartLocalization/SfChartLocalization.iOS/AppDelegate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using Foundation;
+using Syncfusion.SfChart.XForms.iOS.Renderers;
+using UIKit;
+
+namespace SfChartLocalization.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+			//To set localization at the application level
+			//Thread.CurrentThread.CurrentUICulture = new CultureInfo("fr-FR");
+
+			SfChartRenderer.Init();
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
